b6b4aafafbf1378ad8b2a1337f4596b1f1d313ad
kalyanpichuka <kalyanhadoopspark@gmail.com>
2016-06-18 22:49:42 +0530

    Updated ProductDao and HomeController classes

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -1,14 +1,36 @@
 package com.emusicstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.emusicstore.dao.ProductDao;
+import com.emusicstore.model.Product;
 
 @Controller
 public class HomeController {
-	
+
+	// private ProductDao productDao = new ProductDao();
+
+	@Autowired
+	private ProductDao productDao;
+
 	@RequestMapping("/")
-	public String home(){
-		return "home";	
+	public String home() {
+		return "home";
 	}
-	
+
+	@RequestMapping("/productList")
+	public String getProducts(Model model) {
+
+		List<Product> productList = productDao.getProductList();
+		Product product = productList.get(0);
+		model.addAttribute(product);
+		return "productList";
+
+	}
+
 }
--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -1,5 +1,30 @@
 package com.emusicstore.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.emusicstore.model.Product;
+
 public class ProductDao {
+
+	private List<Product> productList;
+
+	public List<Product> getProductList() {
+		
+		Product product1 = new Product();
+		product1.setProductName("Guitar");
+		product1.setProductCategory("Insturment");
+		product1.setProductDescription("This is a guitar");
+		product1.setProductPrice(1800);
+		product1.setProductCondition("New");
+		product1.setProductStatus("Active");
+		product1.setUnitStock(9);
+		product1.setProductManufacturer("Frenda");
+		
+		productList = new ArrayList<Product>();
+		productList.add(product1);
+		return productList;
+		
+	}
 
 }
